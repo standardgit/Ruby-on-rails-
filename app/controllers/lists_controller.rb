12c34e7ac66933e42bad438dc367822fdac81c90
Adejare Toluwalope <standardblaise@gmail.com>
2021-07-26 21:03:06 +0100

    Adding more styling to the form.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,7 +25,7 @@ class ListsController < ApplicationController
 
     respond_to do |format|
       if @list.save
-        format.html { redirect_to @list, notice: "List was successfully created." }
+        format.html { redirect_to lists_url, notice: "List was successfully created." }
         format.json { render :show, status: :created, location: @list }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class ListsController < ApplicationController
   def update
     respond_to do |format|
       if @list.update(list_params)
-        format.html { redirect_to @list, notice: "List was successfully updated." }
+        format.html { redirect_to lists_url, notice: "List was successfully updated." }
         format.json { render :show, status: :ok, location: @list }
       else
         format.html { render :edit, status: :unprocessable_entity }
